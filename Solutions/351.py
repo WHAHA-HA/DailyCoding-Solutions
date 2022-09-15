b70e7f00a36fb4368b444c569853a7a1e418a8ee
Happy Life <103522524+WHAHA-HA@users.noreply.github.com>
2022-09-15 06:12:55 +0300

    Update 351.py

--- Solutions/351.py
+++ Solutions/351.py
@@ -63,9 +63,9 @@ SPACE COMPLEXITY: O(sentence x meaning_map)
 """
 
 # NOTE: The problem is incomplete.
-# We also need a source for which word appears in which context, to be able to infer
-# this in the actual sentences.
-# Once we have a set of strongly correlated words with each word-sense, we can search
-# the context of a word in the target sentence.
-# If there is a high overlap of those words with the already correlated words for a
-# particular word sense, we can guess that that is the answer.
+# We also need a source for which word appears in which context, 
+# to be able to infer this in the actual sentences.
+# Once we have a set of strongly correlated words with each word-sense,
+# we can searchthe context of a word in the target sentence.
+# If there is a high overlap of those words with the already correlated words 
+# for a particular word sense, we can guess that that is the answer.
